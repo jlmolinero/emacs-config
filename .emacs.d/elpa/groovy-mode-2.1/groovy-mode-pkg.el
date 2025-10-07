0a0a01e871dbe9253d669c987ba11816d06f2b14
jlmolinero <javierlopezmolinero@gmail.com>
2025-10-07 12:11:58 +0200

    Adding Groovy mode

---
+++ .emacs.d/elpa/groovy-mode-2.1/groovy-mode-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "groovy-mode" "2.1"
+  "Major mode for Groovy source files."
+  '((s     "1.12.0")
+    (emacs "24.3")
+    (dash  "2.13.0"))
+  :url "https://github.com/Groovy-Emacs-Modes/groovy-emacs-modes"
+  :commit "99eaf70720e4a6337fbd5acb68ae45cc1779bdc4"
+  :revdesc "99eaf70720e4"
+  :keywords '("languages")
+  :authors '(("Russel Winder" . "russel@winder.org.uk")
+             ("Jim Morris" . "morris@wolfman.com")
+             ("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainers '(("Russel Winder" . "russel@winder.org.uk")))
